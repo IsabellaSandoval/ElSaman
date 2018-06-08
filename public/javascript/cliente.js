ffa1b42ec70bd439e623999706aa3330ab621338
Isabella <burbanojaime98@gmail.com>
2018-06-08 15:49:06 -0500

    solucion de errores

--- public/javascript/cliente.js
+++
@@ -1,9 +0,0 @@
-// carrito
-function actualizarCarrito (){
-    document.querySelector('.carrito').innerHTML = arreglo.length;
-}
-
-var arreglo = JSON.parse(localStorage.getItem('arreglo'));
-if(arreglo == null) arreglo = [];
-
-actualizarCarrito();
