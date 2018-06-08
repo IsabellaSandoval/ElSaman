b637937f65fa281bf20e3608a6e702882bf01946
Isabella <burbanojaime98@gmail.com>
2018-06-08 16:41:33 -0500

    error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '.../public'));
+app.use(express.static('public'));
 
 
 // Conectarse a Base de Datos
@@ -75,7 +75,7 @@ app.get('/genetica/terneras', (req, res) => {
 
     ternerasCollection.toArray((err, result) => {
 
-        res.render('VisualizacionArticulos', {
+        res.render('terneras', {
             Terneras: result
         });
     })
