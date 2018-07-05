6e068ee0c3bfe2b1a259f207f603063caf8b53a5
Isabella <burbanojaime98@gmail.com>
2018-07-04 20:36:53 -0500

    contacto funcionando prueba 1.0

---
+++ enviar.php
@@ -0,0 +1,15 @@
+<?php
+
+  $destino: "burbanojaime98@gmail.com";
+  $nombre: $_POST["nombre"];
+  $apellido: $_POST["apellido"];
+  $empresa: $_POST["empresa"];
+  $ciudad: $_POST["ciudad"];
+  $telefono: $_POST["telefono"];
+  $correo: $_POST["correo"];
+  $asunto: $_POST["asunto"];
+  $mensaje: $_POST["mensaje"];
+
+  $contenido: "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nEmpresa : " $empresa . "\nCiudad: " . $ciudad . "\nTélefono: " . $telefono . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje
+mail($destino, $asunto, $contenido);
+?>
